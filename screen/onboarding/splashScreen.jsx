7511031a626f4fee46d39f097a139fe9391786a5
Mustufa <mustufakhanofficial97@gmail.com>
2024-05-08 19:53:54 +0500

    first time push

---
+++ screen/onboarding/splashScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const splashScreen = () => {
+  return (
+    <>
+      <Text>home</Text>
+    </>
+  )
+}
+
+export default splashScreen
